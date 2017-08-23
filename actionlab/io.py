15381917472131432b27111a1ae10f87abf556e8
Dan Gale <danjgale@gmail.com>
2017-08-23 13:55:54 -0600

    added get_data_start()

--- actionlab/io.py
+++ actionlab/io.py
@@ -14,6 +14,49 @@ def get_trial(df, block, trial_number, block_col='BlockNumber',
      columns are consistent across experiments"""
     return df.groupby([block_col, trial_col]).get_group((block, trial_number))
 
+def get_data_start(fn, verify_with='HeaderLines', add=1, sep='\t'):
+    """Reads first line of a data file to determine header rows in file.
+    
+    The first line of a data file is typically a 'HeaderLines' variable, which
+    indicates the number of lines dedicated to headers. This may vary from 
+    trial to trial (i.e. 'task' trials versus 'pause' trials).  
+    
+    Parameters:
+    -----------
+    fn : str
+        File name/path.
+    verify_with : str
+        Identifier to verify that the first line refers to the number of header
+        lines (default is 'HeaderLines', which is standard for data files).
+    add : int
+        Number of lines/rows to add to header lines so that it refers to the 
+        line in which the data appears rather than the headers stop (default is
+        1, which is most common for data files).
+    sep : str
+        Delimiter for data file (default is '\t').
+
+    Returns:
+    --------
+    int
+        Line number in the data file in which the data column headers 
+        begin 
+    """
+    header_df = pd.read_csv(fn, header=None, nrows=1, sep=sep)
+
+    if header_df[0].loc[0] != verify_with:
+        raise Exception(
+                'No {} found in first line. Check file.'.format(verify_with)
+              )
+    
+    try:
+        lines = int(header_df[1])
+    except ValueError:
+        raise ValueError("Number of lines not able to convert to an integer. "
+                         "Check file to verify value.")
+    
+    # add so that lines now indicates the row in which the data starts
+    return lines + add
+
 class DataFile:
     def __init__(self, fn, data_start, sep='\t'):
         """ Handles single-trial data from a single .dat file.
@@ -52,7 +95,7 @@ class DataFile:
         self.data = pd.read_csv(fn, sep=sep, skiprows=data_start - 2)
         
 class SubjectData:
-    def __init__(self, path, data_start, sep='\t'):
+    def __init__(self, path, data_start=None, sep='\t'):
         """ Class containing all trial data belonging to a single subject. 
 
         Main class that assembles all of a subject's data, with variety of 
@@ -86,6 +129,10 @@ class SubjectData:
         self.path = path
         self.id = os.path.basename(os.path.normpath(path))
         self.files = [i for i in os.listdir(self.path) if i.endswith('.dat')]
+
+        if 
+
+
         # read in list of data file objects
         self.data_list = [DataFile(os.path.join(self.path, i), data_start) 
                                for i in self.files]
