ca2432832779cf6d5e0bf49bc55a619a79c3a900
Dan Gale <danjgale@gmail.com>
2018-01-09 17:24:49 -0500

    added glm

--- actionlab/fmri/__init__.py
+++ actionlab/fmri/__init__.py
@@ -1,6 +1,7 @@
 from .preprocess import Preprocessor
 from .normalize import Normalizer
 from .utils import RunManager, ROIExtractor, is_motion_corrected, get_volumes
+from .glm import GLM
 
 __all__ = ['Preprocessor', 'Normalizer', 'RunManager', 'ROIExtractor',
-           'is_motion_corrected', 'get_volumes']
+           'is_motion_corrected', 'get_volumes', 'GLM']
