0c4dd1eca4f4f8d72c4f9d666bfa217ed19b29ca
Dan Gale <danjgale@gmail.com>
2018-01-19 16:30:02 -0500

    added reg report

--- actionlab/fmri/__init__.py
+++ actionlab/fmri/__init__.py
@@ -1,7 +1,7 @@
 from .preprocess import Preprocessor
-from .normalize import Normalizer
+from .normalize import Normalizer, registration_report
 from .run_manager import RunManager, is_motion_corrected, get_volumes
 from .glm import GLM
 
-__all__ = ['Preprocessor', 'Normalizer', 'RunManager',
+__all__ = ['Preprocessor', 'Normalizer', 'registration_report' 'RunManager',
            'is_motion_corrected', 'get_volumes', 'GLM']
