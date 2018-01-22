babf2a3348d37025e111b7efc3506ae7a3bccfd3
Dan Gale <danjgale@gmail.com>
2018-01-21 21:20:13 -0500

    initial commit with basic functions

---
+++ actionlab/fmri/converters.py
@@ -0,0 +1,42 @@
+
+
+import sys
+import os
+import re
+import numpy as np
+import nipype
+from nipype.interfaces.dcm2nii import Dcm2niix
+from traitlets import TraitError
+
+def _make_path(path, return_str=False):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        pass
+
+    if return_str:
+        return tmp_path
+
+# Use dcm2bids package (pip installable) for dicom to bids format
+
+def convert_to_nifti(input_dir, output_dir, compress=True):
+
+    if compress:
+            compress_flag = 'i'
+        else:
+            compress_flag = 'n'
+
+    _make_path(output_dir)
+
+    try:
+        Dcm2niix(
+            source_dir=input_dir,
+            output_dir=output_dir,
+            out_filename='%t%p%s',
+            compress=compress_flag,
+            single_file=True
+        ).run()
+    except Exception as e:
+        # Will get a trait error after each participant
+        print('{} occured; passing...'.format(e))
+        pass
