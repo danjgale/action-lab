7bf9b99c8652bb2df5ce590ed93d59020dba5e08
Dan Gale <danjgale@gmail.com>
2018-01-29 00:34:03 -0500

    added expand events and event duration stuff; to be cleaned up shortly

--- actionlab/experiment/protocol.py
+++ actionlab/experiment/protocol.py
@@ -3,6 +3,8 @@ modelling and time-series indexing.
 """
 
 import pandas as pd
+import numpy as np
+
 
 def _write_event_file(df, event, event_column_names, run, file_pattern,
                       save_path, split=False):
@@ -18,6 +20,75 @@ def _write_event_file(df, event, event_column_names, run, file_pattern,
         all_events.to_csv(
             os.path.join(save_path, '{}_run{}.txt'.format(event, run), sep='\t')
         )
+
+def _compute_durations(start, stop):
+    """Get duration of event based on BVs. Add 1 because duration is inclusive"""
+    return (stop - start) + 1
+
+def get_event_durations(df, start, stop):
+    df['Duration'] = (
+        df.apply(lambda x: _compute_durations(x[start], x[stop]), axis=1)
+    )
+    return df
+
+def expand_events(df, duration_col):
+
+    return df.reindex(
+        np.repeat(df.index.values, df[duration_col].astype(int)), method='ffill'
+    ).reset_index(drop=True)
+
+
+# class ProtocolBase:
+
+#     def __init__(self, onset_col, as_seconds=False):
+
+#         self.onset_col = onset_col
+
+
+#         self.__as_seconds = as_seconds
+
+#     def insert_amplitudes(self, value=1):
+
+#         for i in self.protocols:
+#             i['amp'] = value
+
+#         return self
+
+#     def get_event_durations():
+
+
+
+#     def convert_to_secs(self, columns=):
+
+#         self.__as_seconds = True
+
+#         for i in self.protocols:
+#             i[self.onset_col] = (i[self.onset_col] * 2) - 2
+#             i[self.duration_col] = self.duration_col * 2
+
+#         return self
+
+
+#     def combine(self):
+
+#         concat_list = []
+#         total = 0
+#         for i in protocol_list:
+#             i[self.onset_col] = i[self.onset_col] + total_seconds
+#             total += (i.iloc[-1][self.onset_col] + i.iloc[-1][self.duration_col])
+#             concat_list.append(i)
+
+#         return pd.concat(concat_list, axis=0)
+
+#     def make_event_files(self, event, individual_events=False):
+
+#         for i in self.protocols:
+#             _write_event_file(i, event, [self.onset_col, self.duration_col, 'amp'],
+#                               run, file_pattern, save_path, split=False)
+
+
+
+
 
 
 class ProtocolManager:
@@ -69,3 +140,10 @@ class ProtocolManager:
         for i in self.protocols:
             _write_event_file(i, event, [self.onset_col, self.duration_col, 'amp'],
                               run, file_pattern, save_path, split=False)
+
+
+
+
+# class BVProtocol:
+
+#     def __init__(self, start_col, end_col, condition_col):
