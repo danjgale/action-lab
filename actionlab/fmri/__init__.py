340df9cb38e9f5f0a1b52c53f94bd5ed4044163d
Dan Gale <danjgale@gmail.com>
2018-02-06 18:44:21 -0500

    added changes from preprocess

--- actionlab/fmri/__init__.py
+++ actionlab/fmri/__init__.py
@@ -1,4 +1,4 @@
-from .preprocess import Preprocessor
+from .preprocess import Preprocessor, spatially_smooth, Filter
 from .normalize import Normalizer, registration_report, MNI152_T1_2mm_config
 from .run_manager import (
     RunManager, is_motion_corrected, get_volumes, get_run_numbers
@@ -7,7 +7,7 @@ from .glm import GLM
 from .converters import convert_to_nifti
 from .roi import GlasserExtractor, binarize_mask_array
 
-__all__ = ['Preprocessor', 'Normalizer', 'registration_report' 'RunManager',
-           'is_motion_corrected', 'get_volumes', 'get_run_numbers', 'GLM',
-           'convert_to_nifti', 'MNI152_T1_2mm_config', 'GlasserExtractor',
-           'binarize_mask_array']
+__all__ = ['Preprocessor', 'spatially_smooth', 'Filter', 'Normalizer',
+           'registration_report', 'RunManager', 'is_motion_corrected',
+           'get_volumes', 'get_run_numbers', 'GLM', 'convert_to_nifti',
+           'MNI152_T1_2mm_config', 'GlasserExtractor', 'binarize_mask_array']
