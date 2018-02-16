bdfc3b7287e64fd7a64c98a78bbd8801f464dad3
Dan Gale <danjgale@gmail.com>
2018-02-16 11:45:12 -0500

    added option of setting baseline in percent sig change

--- actionlab/fmri/timecourse.py
+++ actionlab/fmri/timecourse.py
@@ -10,12 +10,15 @@ import pandas as pd
 import scipy
 import nilearn
 
-def percent_signal_change(x):
+def percent_signal_change(x, baseline_index=None):
     """Compute percent signal change for each voxel (column) in array.
     Baseline must be specified and x must be a time * voxel array.
     """
-    # baseline_means = np.mean(x[baseline_index, :], axis=0)
-    signal_mean = np.mean(x, axis=0)
+
+    if baseline_index is not None:
+        signal_mean = np.mean(x[baseline_index, :], axis=0)
+    else:
+        signal_mean = np.mean(x, axis=0)
     signal = 100 * ((x - signal_mean)/signal_mean)
     return signal
 
