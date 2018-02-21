09fee9ba6d6b8bdef919a4ff5a3265b822d20980
Dan Gale <danjgale@gmail.com>
2018-02-21 14:19:24 -0500

    added subject confounds

--- actionlab/fmri/__init__.py
+++ actionlab/fmri/__init__.py
@@ -1,4 +1,4 @@
-from .preprocess import Preprocessor, spatially_smooth, Filter
+from .preprocess import Preprocessor, spatially_smooth, Filter, SubjectConfounds
 from .normalize import Normalizer, registration_report, MNI152_T1_2mm_config
 from .run_manager import (
     RunManager, is_motion_corrected, get_volumes, get_run_numbers
@@ -8,8 +8,8 @@ from .converters import convert_to_nifti
 from .roi import GlasserAtlas, binarize_mask_array, ROIDirectory
 from .timecourse import percent_signal_change
 
-__all__ = ['Preprocessor', 'spatially_smooth', 'Filter', 'Normalizer',
-           'registration_report', 'RunManager', 'is_motion_corrected',
+__all__ = ['Preprocessor', 'spatially_smooth', 'Filter', 'SubjectConfounds',
+           'Normalizer', 'registration_report', 'RunManager', 'is_motion_corrected',
            'get_volumes', 'get_run_numbers', 'GLM', 'convert_to_nifti',
            'MNI152_T1_2mm_config', 'GlasserAtlas', 'binarize_mask_array',
            'ROIDirectory', 'percent_signal_change']
