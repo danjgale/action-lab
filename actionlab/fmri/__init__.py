f5a2d56dd0bfc8ef27d92cb6aa8a05cbb19ae2f1
Dan Gale <danjgale@gmail.com>
2018-02-28 17:18:36 -0500

    added sphere roi and reformatted init for ease of use

--- actionlab/fmri/__init__.py
+++ actionlab/fmri/__init__.py
@@ -5,11 +5,25 @@ from .run_manager import (
 )
 from .glm import GLM
 from .converters import convert_to_nifti
-from .roi import GlasserAtlas, binarize_mask_array, ROIDirectory
+from .roi import GlasserAtlas, binarize_mask_array, ROIDirectory, sphere_mask
 from .timecourse import percent_signal_change
 
-__all__ = ['Preprocessor', 'spatially_smooth', 'Filter', 'SubjectConfounds',
-           'Normalizer', 'registration_report', 'RunManager', 'is_motion_corrected',
-           'get_volumes', 'get_run_numbers', 'GLM', 'convert_to_nifti',
-           'MNI152_T1_2mm_config', 'GlasserAtlas', 'binarize_mask_array',
-           'ROIDirectory', 'percent_signal_change']
+__all__ = [
+    'Preprocessor',
+    'spatially_smooth',
+    'Filter',
+    'SubjectConfounds',
+    'Normalizer',
+    'registration_report',
+    'RunManager',
+    'is_motion_corrected',
+    'get_volumes',
+    'get_run_numbers',
+    'GLM',
+    'convert_to_nifti',
+    'MNI152_T1_2mm_config',
+    'GlasserAtlas',
+    'binarize_mask_array',
+    'ROIDirectory',
+    'sphere_mask',
+    'percent_signal_change']
