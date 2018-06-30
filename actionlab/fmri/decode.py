fixed up scaling and integrated RFE option

actionlab/fmri/decode.py
@@ -7,6 +7,8 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, auc, confusion_matrix
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn import feature_selection
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def _standardize(train_x, test_x, scaling):
@@ -41,16 +43,27 @@ def _min_max(train_x, test_x, scaling, scale_min=-1, scale_max=1):
     return train_x, test_x
 
 
-def _classify(classifier, train_x, train_y, test_x, test_y, scaling=None,
-              standardize=True, scale_min=-1, scale_max=1):
+def _scale(train_x, test_x, scaler, direction='voxels'):
+    """Pass in a scikit-learn scaling object (e.g., StandardScaler) to scale
+    training data and test data (separately, based on the training data)"""
+    if direction == 'voxels':
+        train_x = scaler.fit_transform(train_x)
+        test_x = scaler.transform(test_x)
+    elif direction == 'pattern':
+        # scale within pattern (each trial pattern gets standardized). Scaling is
+        # different for each sample/pattern and therefore you cannot transform your
+        # test set based on your training set
+        train_x = zscore(train_x, axis=1)
+        print(np.std(train_x, axis=1).shape)
+        test_x = zscore(test_x, axis=1)
+    return train_x, test_x
+
+
+def _classify(classifier, train_x, train_y, test_x, test_y, scaler=None,
+              scale_direction='voxels'):
     """Train and evaluate SVM classifier"""
 
     # apply scaling
-    if scaling is not None:
-        if standardize:
-            train_x, test_x = _standardize(train_x, test_x, scaling)
-        else:
-            train_x, test_x = _min_max(train_x, test_x, scaling, scale_min, scale_max)
 
     classifier.fit(train_x, train_y)
     yhat = classifier.predict(test_x)
@@ -58,8 +71,8 @@ def _classify(classifier, train_x, train_y, test_x, test_y, scaling=None,
 
 
 def leave_one_run_out(df, classifier, run_column='run', data_column='voxels',
-                      response_column='condition', scaling='voxel',
-                      shuffle_data=True, standardize=True, scale_min=-1, scale_max=1):
+                      response_column='condition', scaler=None,
+                      scale_direction='voxel', rfe_voxels=None, shuffle_data=True):
     """Perform leave one run out cross-validation"""
 
     if df[response_column].dtype != np.int:
@@ -89,8 +102,21 @@ def leave_one_run_out(df, classifier, run_column='run', data_column='voxels',
         train_x = np.vstack(train_subset[data_column]).astype(np.float)
         train_y = train_subset[response_column].values
 
-        accuracy, yhat, model = _classify(classifier, train_x, train_y, test_x, test_y, scaling=scaling,
-                                   standardize=standardize,  scale_min=scale_min, scale_max=scale_max)
+        if scaler is not None:
+            train_x, test_x = _scale(train_x, test_x, scaler, scale_direction)
+
+        # if RFE, identify and select top N features based on training data only
+        if rfe_voxels is not None:
+            rfe_model = classifier
+            selector = feature_selection.RFE(rfe_model, rfe_voxels)
+            selector.fit(train_x, train_y)
+
+            # narrow down training set
+            train_x = train_x[:, selector.support_]
+            # narrow down test set **based on features only determined in test set**
+            test_x = test_x[:, selector.support_]
+
+        accuracy, yhat, model = _classify(classifier, train_x, train_y, test_x, test_y)
 
         # store data require for metrics
         accuracies.append(accuracy)
@@ -134,23 +160,21 @@ def cross_decode(train_data, classifier, test_data, data_column='voxels', respon
 class Decoder:
 
     def __init__(self, data, classifier=SVC(C=1, kernel='linear'), run_column='run',
-                 data_column='voxels', response_column='condition', scaling='voxel',
-                 standardize=True, scale_min=-1, scale_max=1):
+                 data_column='voxels', response_column='condition', scaler=StandardScaler(),
+                 scale_direction='voxel'):
 
         self.data = data
         self.classifier = classifier
         self.run_column = run_column
         self.data_column = data_column
         self.response_column = response_column
-        self.scaling = scaling
-        self.standardize = standardize
-        self.scale_min = -1
-        self.scale_max = 1
+        self.scaler = scaler
+        self.scale_direction = scale_direction
 
         self.n_classes = len(np.unique(self.data[self.response_column]))
 
 
-    def cross_validate(self, shuffle_data=True, conditions=None):
+    def cross_validate(self, shuffle_data=True, conditions=None, rfe_voxels=None):
 
         if conditions is not None:
             self.cv_data = self.data[self.data[self.response_column].isin(conditions)]
@@ -161,10 +185,10 @@ class Decoder:
             leave_one_run_out(self.cv_data, self.classifier, run_column=self.run_column,
                               data_column=self.data_column,
                               response_column=self.response_column,
-                              scaling=self.scaling,
+                              scaler=self.scaler,
+                              scale_direction=self.scale_direction,
                               shuffle_data=shuffle_data,
-                              standardize=self.standardize,
-                              scale_min=self.scale_min, scale_max=self.scale_max)
+                              rfe_voxels=rfe_voxels)
         )
 
         self.mean_accuracy = np.mean(self.accuracies)