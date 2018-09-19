a3a1e71ff306dad7ff99144529736ce514628a4a
Dan Gale <danjgale@gmail.com>
2018-09-19 15:36:40 -0400

    updated dependencies and setup requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,25 @@ from distutils.core import setup
 
 setup(
     name='actionlab',
-    version='0.1dev',
+    version='0.0.1',
     packages=['actionlab'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='MIT',
     author='Dan Gale',
-    long_description=open('README.md').read(), # PyPi description
+    long_description=open('README.md').read(),
     url='https://github.com/danjgale/action-lab/',
-    tests_require=['pytest']
-    # install_requires=[],
-    # setup_requires=['pytest-runner'],
-    # tests_require=['pytest'],
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ]
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'scikit-learn',
+        'scikit-image',
+        'nilearn',
+        'nipype'
+    ]
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
